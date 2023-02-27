909745bd7e053c7bc72c7f5052d82ebd4433e397
web2ls <web2ls@yandex.ru>
2023-02-27 21:23:05 +0400

    init project

---
+++ board.js
@@ -0,0 +1,10 @@
+class Board {
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.grid = this.getEmptyBoard();
+  }
+
+  getEmptyBoard() {
+    return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+  }
+}
